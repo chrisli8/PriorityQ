ca4f214036f4f438e6ba494d16599d967f8c0fa3
eduardrg <eduardrg@uw.edu>
2017-03-29 17:20:09 -0700

    Implement Comparable so that TaskItem can be used with PriorityQueue

--- app/src/main/java/priorityq/chrisli8/washington/edu/priorityq/TaskItem.java
+++ app/src/main/java/priorityq/chrisli8/washington/edu/priorityq/TaskItem.java
@@ -1,10 +1,14 @@
 package priorityq.chrisli8.washington.edu.priorityq;
 
+import android.support.annotation.NonNull;
+
+import java.util.Comparator;
+
 /**
  * Created by ChrisLi on 3/27/17.
  */
 
-public class TaskItem {
+public class TaskItem implements Comparable<TaskItem>{
     private int priority;
     private int duration;
     private String name;
@@ -32,5 +36,27 @@ public class TaskItem {
 
     public int getDuration() {
         return this.duration;
+    }
+
+    @Override
+    // compare priorities
+    // if this task has higher priority than ti it is "bigger"
+    public int compareTo(@NonNull TaskItem ti) {
+        if (this.getPriority() < ti.getPriority()) {
+            return -1;
+        } else if (this.getPriority() > ti.getPriority()) {
+            return 1;
+        // priorities are equal, so compare duration
+        // i.e. if this task is shorter than ti it is "bigger"
+        } else {
+            if (this.getDuration() < ti.getDuration()) {
+                return 1;
+            } else if (this.getDuration() > ti.getDuration()) {
+                return -1;
+            // Priority and duration are the same, so compare names
+            } else {
+                return this.getTaskName().compareTo(ti.getTaskName());
+            }
+        }
     }
 }
