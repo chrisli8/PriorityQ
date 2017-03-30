f822318c3648e68526bb818a8b95a4beae86804e
eduardrg <eduardrg@uw.edu>
2017-03-29 17:18:15 -0700

    Refactor to use SQLiteDatabase to store tasks

--- app/src/main/java/priorityq/chrisli8/washington/edu/priorityq/MainActivity.java
+++ app/src/main/java/priorityq/chrisli8/washington/edu/priorityq/MainActivity.java
@@ -1,5 +1,7 @@
 package priorityq.chrisli8.washington.edu.priorityq;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,12 +12,16 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-    private MyHeap tasks;
     private FragmentManager fragManager;
-
+    private SQLiteDatabase db;
+    private PriorityQueue<TaskItem> taskItems;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -57,10 +63,40 @@ public class MainActivity extends AppCompatActivity {
                 .replace(R.id.content, new DequeueTaskFragment())
                 .commit();
 
-        tasks = new MyHeap();
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        this.db = openOrCreateDatabase("TASK_DB", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS TASK (taskId INTEGER PRIMARY KEY AUTOINCREMENT, taskName VARCHAR, taskPriority SMALLINT, taskDuration INT)");
+    }
+
+    // currently vulnerable to SQL injection
+    void saveTaskToDB(String taskName, int taskPriority, int taskDuration) {
+        db.execSQL("INSERT INTO TASK (taskName, taskPriority, " +
+                "taskDuration) VALUES (\"" + taskName + "\", " +
+                taskPriority + ", " + taskDuration + ");");
+    }
+
+    // Return a List of all TaskItems in the DB
+    List<TaskItem> getAllTasksFromDB() {
+        Cursor c = this.db.rawQuery("SELECT * FROM TASK", null);
+        final List<TaskItem> list = new ArrayList<>();
+        try {
+            while (c.moveToNext()) {
+                String taskName = c.getString(c.getColumnIndex("taskName"));
+                int taskPriority = c.getInt(c.getColumnIndex
+                        ("taskPriority"));
+                int taskDuration = c.getInt(c.getColumnIndex
+                        ("taskDuration"));
+                TaskItem taskItem = new TaskItem(taskPriority, taskDuration,
+                        taskName);
+                list.add(taskItem);
+            }
+        } finally {
+            c.close();
+        }
+        return list;
     }
 
 }
